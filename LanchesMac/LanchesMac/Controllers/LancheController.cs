b42dc7cb81c5f1d7fca8d0d784cb919146ddbdb1
Lucas Antunes <lucasdanielantunes@gmail.com>
2021-12-02 20:22:15 -0300

    Atualização do projeto

---
+++ LanchesMac/LanchesMac/Controllers/LancheController.cs
@@ -0,0 +1,23 @@
+﻿using LanchesMac.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Controllers
+{
+    public class LancheController : Controller
+    {
+        private readonly ICategoriaRepository _categoriaRepository;
+        private readonly ILancheRepository _lancheRepository;
+        public LancheController(ICategoriaRepository categoriaRepository, ILancheRepository lancheRepository)
+        {
+            _categoriaRepository = categoriaRepository;
+            _lancheRepository = lancheRepository;
+        }
+
+        public IActionResult List()
+        {
+            var lanches = _lancheRepository.Lanches;
+            return View(lanches);
+        }
+
+    }
+}
---
+++ LanchesMac/LanchesMac/Repositories/Interfaces/ILancheRepository.cs
@@ -0,0 +1,11 @@
+﻿using LanchesMac.Models;
+
+namespace LanchesMac.Repositories.Interfaces
+{
+    public interface ILancheRepository
+    {
+        IEnumerable<Lanche> Lanches { get; }
+        IEnumerable<Lanche> LanchePreferido { get; }
+        Lanche GetLancheById(int lancheId);
+    }
+}
