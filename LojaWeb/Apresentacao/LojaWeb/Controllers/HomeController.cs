b3488a850fbb3c985d45016574ab71a8927d5f76
Lucas Antunes <lucasdanielantunes@gmail.com>
2021-12-15 14:04:12 -0300

    Atualização

--- LojaWeb/Apresentacao/LojaWeb/Controllers/HomeController.cs
+++ LojaWeb/Apresentacao/LojaWeb/Controllers/HomeController.cs
@@ -11,13 +11,6 @@ namespace LojaWeb.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
@@ -28,10 +21,6 @@ namespace LojaWeb.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        
     }
 }
